4e81eddebbedb4b7fffaed77427e9a0f7ffafc5f
Latief <latifftrahman@gmail.com>
2025-12-06 19:58:21 +0700

    perbaikan db js 2

--- db.js
+++ db.js
@@ -2,15 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    // Aiven MySQL biasanya pakai sertifikat root default
-    rejectUnauthorized: true
-  }
+  uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl-mode=VERIFY_CA`
 });
 
 connection.connect((err) => {
