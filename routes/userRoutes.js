e36b5c71174bfb9e87677b626c334519fc909826
Latief <latifftrahman@gmail.com>
2025-12-13 00:48:47 +0700

    tes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authenticateToken = require('../middleware/authenticateToken');
 
-router.get('/users/:id', userController.getUserById); 
 router.get('/users', authenticateToken, userController.getAllUsers);
+router.get('/users/:id', userController.getUserById); 
 router.delete('/users/:id', authenticateToken, userController.deleteUser);
 
 module.exports = router;
